1f2c207e37b48dcb2690bc70b8fb1f8300c79941
Ziquan Wang <aresdrw@163.com>
2025-06-04 21:28:48 +0800

    Add files via upload

--- dehaze_seg/models/backbones/__init__.py
+++ dehaze_seg/models/backbones/__init__.py
@@ -1,1 +1,12 @@
+""" 
+-*- coding: utf-8 -*-
+    @Time    : 2025/4/9  14:56
+    @Author  : AresDrw
+    @File    : __init__.py.py
+    @Software: PyCharm
+    @Describe: 
+-*- encoding:utf-8 -*-
+"""
+from .dehazeformer import DehazeFormer
 
+__all__ = ['DehazeFormer']
